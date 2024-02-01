d2ef4517e52d5dc4b469ade60e2f249c1766820f
superruudje <rjhaamke@gmail.com>
2024-02-02 00:39:24 +0100

    feat: added duration + removed old code

--- src/components/cardComponent.vue
+++ src/components/cardComponent.vue
@@ -3,7 +3,8 @@
         <img :src="images[`${fileName}`] || images[`default`]" class="card-img-top" alt="thumbnail">
         <div class="card-body">
             <h6 class="card-title">{{ card.title }}</h6>
-            <small class="fst-italic">{{ card.date }}</small>
+            <div class="fst-italic">{{ card.date }}</div>
+            <div class="fst-italic">{{ duration }}</div>
             <div class="d-flex">
                 <a class="text-danger me-2" v-if="card.type === 'stream' && card.youtube" :href="card.youtube" target="_blank"><i class="bi bi-youtube"></i></a>
                 <a class="text-black me-2" v-if="card.type === 'stream' && card.twitch" :href="card.twitch" target="_blank"><i class="bi bi-twitch"></i></a>
@@ -33,6 +34,28 @@ const fileName = computed(() => {
     } else
         return GetYoutubeIDFromURL(props.card.url)
 })
+
+const duration = computed(() => {
+    switch (props.card.type) {
+        case 'stream':
+            return ConvertMinutesToTime(props.card.total_duration)
+        case 'video':
+            return props.card.duration
+        case 'podcast':
+            return props.card.duration
+        default:
+            return props.card.duration
+    }
+})
+
+function ConvertMinutesToTime(minutes) {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    const seconds = 0
+
+    return `${hours}:${remainingMinutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
 
 function GetYoutubeIDFromURL(url) {
     let id = url.split('=')[1];
