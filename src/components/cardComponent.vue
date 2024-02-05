000d742bbb5c9216cda6c29a4be5ab0528dde652
superruudje <rjhaamke@gmail.com>
2024-02-06 00:17:10 +0100

    ui: card design.

--- src/components/cardComponent.vue
+++ src/components/cardComponent.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="card h-100 border border-4" @click="selectCard" :class="card.type === 'podcast' ? 'border-success' :
-            card.type === 'video' ? 'border-danger' : 'border-warning'">
-        <div class="position-relative">
+    <div class="card h-100 border-0 bg-transparent" @click="selectCard">
+        <div class="position-relative border border-3 rounded-1" :class="card.type === 'podcast' ? 'border-success' :
+            card.type === 'video' ? 'border-yt' : 'border-tw'">
             <img :src="images[`${imgName}`] || images[`default`]" class="card-img-top" alt="thumbnail">
             <span class="badge rounded-0 bg-secondary position-absolute top-0 start-0"
                   style="--bs-bg-opacity: .75;">{{ card.date }}</span>
             <span class="badge rounded-0 bg-secondary position-absolute bottom-0 end-0"
                   style="--bs-bg-opacity: .75;">{{ duration }}</span>
-            <span class="badge rounded-0 position-absolute top-0 end-0 text-uppercase" :class="card.type === 'podcast' ? 'bg-success' :
-            card.type === 'video' ? 'bg-danger' : 'bg-warning'">{{ card.type }}</span>
+            <span class="badge rounded-0 position-absolute top-0 end-0 text-uppercase fw-bold" :class="card.type === 'podcast' ? 'bg-success' :
+            card.type === 'video' ? 'bg-yt' : 'bg-tw'">{{ card.type }}</span>
             <div class="position-absolute bottom-0 start-0">
-                <a :href="'https://youtube.com/watch?v=' + card['youtube_id']" class="btn btn-sm btn-light rounded-0" v-if="card['youtube_id']"><i class="bi bi-youtube text-youtube"></i></a>
-                <a :href="'https://www.twitch.tv/videos/' + card['twitch_id']" class="btn btn-sm btn-light rounded-0" v-if="card['twitch_id']"><i class="bi bi-twitch text-twitch"></i></a>
+                <a :href="'https://www.twitch.tv/videos/' + card['twitch_id']" class="platform-link" v-if="card['twitch_id']"><img src="../assets/img/twitch.png" alt="logo"></a>
+                <a :href="'https://youtube.com/watch?v=' + card['youtube_id']" class="platform-link" v-if="card['youtube_id']"><img src="../assets/img/youtube.png" alt="logo"></a>
             </div>
         </div>
-        <div class="card-body">
-            <h6 class="card-title m-0">{{ title }}</h6>
+        <div class="card-body py-2 px-0">
+            <span class="card-title m-0">{{ title }}</span>
         </div>
     </div>
 </template>
@@ -80,4 +80,7 @@ function selectCard() {
 <style scoped lang="sass">
 .card:hover
     cursor: pointer
+.platform-link img
+    height: 24px
+    width: 24px
 </style>
