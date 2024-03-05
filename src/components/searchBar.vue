5d00b1251625d50f7985a57cc5bed07a1cd183da
superruudje <rjhaamke@gmail.com>
2024-03-05 19:40:30 +0100

    fix: als je vanaf een single item terugging naar home was de zoekterm weer leeg.

--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -4,7 +4,7 @@
             <div class="row py-3">
                 <div class="col-12">
                     <div class="input-group">
-                        <input class="form-control" type="search" placeholder="Zoek een video" v-debounce:300="input">
+                        <input class="form-control" type="search" placeholder="Zoek een video" v-debounce:300="input" :value="store.search">
                         <span class="input-group-text"><i class="bi bi-search"></i></span>
                     </div>
                 </div>
