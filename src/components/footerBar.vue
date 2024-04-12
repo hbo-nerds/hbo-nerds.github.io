316830640478a5b7a4aaede15af4268acde50579
braxshinoa <158345413+braxshinoa@users.noreply.github.com>
2024-04-12 23:00:12 +0200

    update dataverzamelaars van de json

--- src/components/footerBar.vue
+++ src/components/footerBar.vue
@@ -3,7 +3,7 @@
         <footer class="d-flex flex-wrap justify-content-between align-items-baseline py-3">
             <p class="col-12 col-md-6  mb-0 text-body-secondary">Gemaakt door Superruudje & Sheepolution. Bekijk op <a
                 href="https://github.com/lekkersicko/lekker-speuren">Github</a>.</p>
-            <p class="col-12 col-md-6 ms-auto mb-0 text-body-secondary text-md-end">Data verzameld en verwerkt door Sheepolution, Braxshinoa, Donnosaurus, CappiSteijns, en LolligeOllie.</p>
+            <p class="col-12 col-md-6 ms-auto mb-0 text-body-secondary text-md-end">Data verzameld en verwerkt door Sheepolution, Braxshinoa en Donnosaurus.</p>
         </footer>
     </div>
 </template>
