d88dfa968bf5c2e75e9a8856dbdbce8f280bcd57
superruudje <rjhaamke@gmail.com>
2024-05-13 22:32:10 +0200

    change(#82): add footer note.

--- src/components/footerBar.vue
+++ src/components/footerBar.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="container-fluid bg-dark-subtle">
         <footer class="d-flex flex-wrap justify-content-between align-items-baseline py-3">
-            <p class="col-12 col-md-6  mb-0 text-body-secondary">Gemaakt door Superruudje & Sheepolution. Bekijk op <a
-                href="https://github.com/lekkersicko/lekker-speuren">Github</a>.</p>
-            <p class="col-12 col-md-6 ms-auto mb-0 text-body-secondary text-md-end">Data verzameld en verwerkt door Sheepolution, Braxshinoa, Donnosaurus en Rikels.</p>
+            <div class="col-12 col-md-6">
+                <p class="mb-0 text-body-secondary">Gemaakt door Superruudje & Sheepolution. Bekijk op <a
+                    href="https://github.com/lekkersicko/lekker-speuren">Github</a>.</p>
+            </div>
+            <div class="col-12 col-md-6 ms-auto text-md-end">
+                <p class="mb-0 text-body-secondary">Data verzameld en verwerkt door Sheepolution, Braxshinoa, Donnosaurus en Rikels.</p>
+                <span class="text-body-secondary"><i class="bi bi-exclamation-circle me-2"></i>Wij zijn niet geaffilieerd met Lekker Spelen.</span>
+            </div>
         </footer>
     </div>
 </template>
