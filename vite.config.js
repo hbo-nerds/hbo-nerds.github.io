345fb51c1d0813c406cbb9eac706a64640fa7c5e
Sam van de Kreeke <sam@podobrace.nl>
2024-09-19 18:54:22 +0200

    chore: changed format script in package.json and formatted files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
